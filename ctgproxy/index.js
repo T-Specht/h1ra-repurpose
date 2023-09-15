6ac1787f328ba5700191bde812da4fc5723683c2
Tim Specht <t.specht@dres-specht.de>
2023-09-15 11:44:56 +0200

    Updated CSP in proxy

--- ctgproxy/index.js
+++ ctgproxy/index.js
@@ -9,7 +9,15 @@ app.use(
     contentSecurityPolicy: {
       useDefaults: false,
       directives: {
-        defaultSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        // defaultSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        defaultSrc: "* data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'",
+        scriptSrc: "* data: blob: 'unsafe-inline' 'unsafe-eval'",
+        connectSrc: "* data: blob: 'unsafe-inline'",
+        imgSrc: "* data: blob: 'unsafe-inline'",
+        frameSrc: "* data: blob:",
+        styleSrc: "* data: blob: 'unsafe-inline'",
+        fontSrc: "* data: blob: 'unsafe-inline'",
+        //frameAncestors: "* data: blob: 'unsafe-inline'",
         frameAncestors: ["*." + process.env.SITE_ADDRESS],
       },
     },
@@ -21,6 +29,7 @@ app.use(
   createProxyMiddleware({
     target: "https://classic.clinicaltrials.gov/",
     changeOrigin: true,
+    logLevel: 'debug'
   })
 );
 app.listen(3000);
