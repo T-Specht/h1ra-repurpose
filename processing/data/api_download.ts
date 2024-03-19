194a557f43c5b8312659633a65f9dc6cecefe788
Tim Specht <T-Specht@users.noreply.github.com>
2024-03-19 18:58:58 +0100

    allow for rank > 1000

--- processing/data/api_download.ts
+++ processing/data/api_download.ts
@@ -82,10 +82,11 @@ export let fields = [
   "WhyStopped",
 ];
 
+// Increase this number if there are more than 1000 results for the search expression
+const MAX_RANK_LIMIT = 1000;
+
 const params = {
-  expr: "<expr here>",
-  min_rnk: 1,
-  max_rnk: 1000,
+  expr: "<expr>",
   fmt: "json",
 };
 
@@ -93,28 +94,55 @@ export const apiDownload = async (writeFile = true, fileName = './api_results.js
   let chunks = _.chunk(fields, 19);
   console.log(`Downloading in ${chunks.length} chunks.`);
 
-  let results: any[] = [];
 
-  for (let chunk of chunks) {
-    let res = await axios.get(base, {
-      params: {
-        ...params,
-        fields: [...chunk, "NCTId"].join(","),
-      },
-    });
+  let resultsOverAllRanks: any[] = [];
 
-    results = _.merge(results, res.data.StudyFieldsResponse.StudyFields);
-  }
+  let min_rank = 1;
+  let max_rank = Math.min(MAX_RANK_LIMIT, 1000); // if MAX_RANK_LIMIT < 1000
+
+  // First loop is used if there are more than 1000 entries for search and MAX_RANK_LIMIT is set accordingly
+  do {
+
+
+    let results: any[] = [];
+
+
+    for (let chunk of chunks) {
+      let res = await axios.get(base, {
+        params: {
+          ...params,
+          min_rnk: min_rank,
+          max_rnk: max_rank,
+          fields: [...chunk, "NCTId"].join(","),
+        },
+      });
+
+      results = _.merge(results, res.data.StudyFieldsResponse.StudyFields);
+
+    }
+
+    console.log({ min_rank, max_rank, results_length: results.length });
+
+
+    resultsOverAllRanks.push(results);
+
+    if (max_rank >= MAX_RANK_LIMIT) break;
+
+    const rank_diff = Math.min(MAX_RANK_LIMIT - max_rank, 1000);
+    min_rank = max_rank + 1;
+    max_rank += rank_diff;
+  } while (max_rank <= MAX_RANK_LIMIT);
+
 
   if (writeFile) {
     writeFileSync(
       fileName,
-      JSON.stringify({ date: new Date(), data: results }, null, 3)
+      JSON.stringify({ date: new Date(), data: resultsOverAllRanks }, null, 3)
     );
     console.log(`Done. Written to file ${fileName}`);
   }
 
-  return results;
+  return resultsOverAllRanks;
 };
 
 if (require.main === module) {
